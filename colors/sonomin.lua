8a1848e90c93015de4f79466a025fba20dbddd04
Styzex <120598476+Styzex@users.noreply.github.com>
2024-08-18 21:27:58 +0200

    Got rid of the prints

--- colors/sonomin.lua
+++ colors/sonomin.lua
@@ -1,5 +1,3 @@
-print("Loading Sonomin colorscheme...")
-
 if vim.g.colors_name then
     vim.cmd("hi clear")
 end
@@ -9,12 +7,7 @@ vim.g.colors_name = "sonomin"
 
 local ok, sonomin = pcall(require, "sonomin")
 if not ok then
-    print("Failed to load Sonomin module: " .. tostring(sonomin))
     return
 end
 
-print("Sonomin module loaded successfully")
-
 sonomin.setup()
-
-print("Sonomin colorscheme v" .. sonomin.version .. " applied successfully")
